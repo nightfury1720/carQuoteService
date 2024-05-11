f2b4303069665e3a4039018622bacad98afa469f
Sarang Mehrotra <sarangmehrotra@Sarangs-MacBook-Pro.local>
2024-05-11 14:31:19 +0530

    feat: adding-dist-api

--- helper.js
+++ helper.js
@@ -5,3 +5,29 @@ export const quoteCalculator = (time, carHourlyRate, availabilityStatus) => {
         return -1;
     }
 }
+export const calculateDistance = (origin, destination) => {
+    var directionsService = new google.maps.DirectionsService();
+
+    var request = {
+        origin: origin,
+        destination: destination,
+        travelMode: 'DRIVING' 
+    };
+
+    var response = directionsService.route(request);
+
+    if (response && response.status === 'OK') {
+        var route = response.routes[0];
+
+        var totalDistance = 0;
+        for (var i = 0; i < route.legs.length; i++) {
+            totalDistance += route.legs[i].distance.value;
+        }
+
+        totalDistance = totalDistance / 1000;
+
+        return totalDistance;
+    } else {
+        return response.status;
+    }
+}
--- index.js
+++ index.js
@@ -10,19 +10,21 @@ import {
   where,
 } from "firebase/firestore/lite";
 import { quoteCalculator } from "./helper.js";
-
+import { Client } from "@googlemaps/google-maps-services-js";
 const firebaseConfig = {
-    apiKey: "AIzaSyBt_IRgNnPVgggfy8tb89xVSlhJ-jJN2VQ",
-    authDomain: "carservice-637df.firebaseapp.com",
-    projectId: "carservice-637df",
-    storageBucket: "carservice-637df.appspot.com",
-    messagingSenderId: "722836941976",
-    appId: "1:722836941976:web:bdc19fa2f7813d6ff5f4ed",
-    measurementId: "G-60Z8FSBMFE"
-  };
+  apiKey: "AIzaSyBt_IRgNnPVgggfy8tb89xVSlhJ-jJN2VQ",
+  authDomain: "carservice-637df.firebaseapp.com",
+  projectId: "carservice-637df",
+  storageBucket: "carservice-637df.appspot.com",
+  messagingSenderId: "722836941976",
+  appId: "1:722836941976:web:bdc19fa2f7813d6ff5f4ed",
+  measurementId: "G-60Z8FSBMFE",
+};
 
 const firebaseApp = initializeApp(firebaseConfig);
 const db = getFirestore(firebaseApp);
+
+const client = new Client({});
 
 // Get a list of cities from your database
 async function getCars() {
@@ -90,10 +92,14 @@ app.get("/quote/:carId", async (req, res) => {
     const { carId } = req.params;
     const car = await getCarById(carId);
     if (car) {
-      const rentTime = Number(req.query.rentTime); 
+      const rentTime = Number(req.query.rentTime);
       const carHourlyRate = car.hourlyRate;
       const availabilityStatus = car.availability;
-      const quote = quoteCalculator(rentTime, carHourlyRate, availabilityStatus);
+      const quote = quoteCalculator(
+        rentTime,
+        carHourlyRate,
+        availabilityStatus
+      );
       console.log(quote);
       if (quote !== -1) {
         res.json({ quote });
@@ -109,7 +115,39 @@ app.get("/quote/:carId", async (req, res) => {
   }
 });
 
+app.post("/calculate-distance", async (req, res) => {
+  const { originLat, originLng, destinationLat, destinationLng } = req.body;
+  const apiKey = "AIzaSyBoe_uPAfpB82mJ3EsqqeWCCL9v6UiS5Z4";
 
+  try {
+    const response = await client.directions({
+      params: {
+        origin: `${originLat},${originLng}`,
+        destination: `${destinationLat},${destinationLng}`,
+        key: apiKey,
+      },
+    });
+
+    console.log(response.data.routes[0].legs);
+    if (response.data.status === "OK") {
+      const route = response.data.routes[0];
+      let totalDistance = 0;
+      let totalTime =0;
+      for (const leg of route.legs) {
+        totalDistance += leg.distance.value;
+        totalTime += leg.duration.value;
+      }
+      totalDistance = totalDistance / 1000; // Convert meters to kilometers
+      res.json({ distance: totalDistance , time: totalTime});
+    } else {
+      console.error("Error calculating distance:", response.data.status);
+      res.status(500).json({ error: "Internal server error" });
+    }
+  } catch (error) {
+    console.error("Error calculating distance:", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 const PORT = process.env.PORT || 3000;
 
